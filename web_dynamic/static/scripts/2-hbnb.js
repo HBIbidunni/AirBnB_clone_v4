96335b7f7935e539f42a927c455441fc27f0d3c0
Pinerealm <osalami09@gmail.com>
2023-08-07 04:25:28 +0300

    Implement visual API status

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,30 @@
+// This script should be executed only when the DOM is loaded
+$(function () {
+  const amenityDict = {};
+  $('input[type=checkbox]').click(function () {
+    if ($(this).is(':checked')) {
+      amenityDict[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete amenityDict[$(this).attr('data-id')];
+    }
+    $('.amenities h4').text(Object.values(amenityDict).join(', '));
+  });
+}
+);
+
+// Request the url http://0.0.0.0:5001/api/v1/status/:
+// If the status is “OK”, add the class available to the DIV#api_status
+// Otherwise, remove the class available to the DIV#api_status
+$( window ).on( "load", function() {
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data, textStatus) {
+    if (textStatus !== 'success') {
+      if (data.status === 'OK') {
+        $('#api_status').addClass('available');
+      } else {
+        $('#api_status').removeClass('available');
+      }
+    }
+  }
+  );
+}
+);
